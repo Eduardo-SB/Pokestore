9e037b0383007248badb5c7ef1493d37a3ea52cc
Eduardo SB <eduardosb95@gmail.com>
2020-07-08 20:27:15 -0300

    Updates

---
+++ precache-manifest.d22e9769d4e251859dcfe1232a8a8466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ce90adc076b18e8b364112117fdb3a",
+    "url": "/Pokestore/index.html"
+  },
+  {
+    "revision": "f3927a909088c572e15b",
+    "url": "/Pokestore/static/css/main.45fa083f.chunk.css"
+  },
+  {
+    "revision": "fb6fad9781f80b67d0b2",
+    "url": "/Pokestore/static/js/2.479feba4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokestore/static/js/2.479feba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3927a909088c572e15b",
+    "url": "/Pokestore/static/js/main.2958a523.chunk.js"
+  },
+  {
+    "revision": "8e764dead43e18c720d3",
+    "url": "/Pokestore/static/js/runtime-main.5166b406.js"
+  },
+  {
+    "revision": "978caf8f0d41385e220c711059e84218",
+    "url": "/Pokestore/static/media/pikachu.978caf8f.png"
+  },
+  {
+    "revision": "c285788d70abbb0ad7dad6fbf4d49c0d",
+    "url": "/Pokestore/static/media/pokestore.c285788d.png"
+  }
+]);
