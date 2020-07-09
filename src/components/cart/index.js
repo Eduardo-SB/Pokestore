67638b9f0295362b25cee5f66fcfa64c8876ec36
Eduardo SB <eduardosb95@gmail.com>
2020-07-08 23:29:32 -0300

    Add cart custom

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -4,6 +4,11 @@ import './styles.css';
 function chart() {
   return <>
     <div className="cart-pokemon">
+      <h1 className="title-cart">Carrinho</h1>
+      <div className="list-items">
+        
+      </div>
+      <button className="button-cart">Finalizar</button>
     </div>
   </>;
 }
--- src/components/catalog/index.js
+++ src/components/catalog/index.js
@@ -6,91 +6,42 @@ import { FaShoppingCart } from 'react-icons/fa'
 
 function Catalog() {
   const [pokemon, setPokemon] = useState([]);
+  const [imgPokemon, setImgPokemon] = useState([]);
+  const [infoPokemon, setInfoPokemon] = useState([]);
   
   useEffect(() => {
 
-    setPokemon(axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=200"))
-  // .then(function(response){
-  //   console.log(response.data); // ex.: { user: 'Your User'}
-  //   console.log(response.status); // ex.: 200
-  // });  
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=12&offset=200").then( res => { 
+    setPokemon(res.data.results) 
     
+    res.data.results.map( res =>(
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${res.name}`).then( resInfo => {
+        setImgPokemon(imgPokemon.concat(resInfo.data.sprites.back_default))
+        setInfoPokemon(resInfo.data.abilities[0].ability.name);
+       }) 
+    ))
+  })
   }, []);
-
+  
   return <>
     <div className="catalog-pokemon">
 
-    <div className="card-pokemon">
-      <div className="card-pokemon-text">
-        <img className="img-pokemon" src="https://m.media-amazon.com/images/S/aplus-media/vc/cab6b08a-dd8f-4534-b845-e33489e91240._CR75,0,300,300_PT0_SX300__.jpg" alt=""/>
-        <div className="title-card-pokemon">
-          <h2>Morgan Sweeney</h2>
-          <div className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none
-            in their pants.</div>
-          <div className="actions">
-            <button>Comprar <FaShoppingCart /></button>
+    { pokemon.map( poke => (
+      <div key={poke.name}>
+        <div className="card-pokemon">
+        <div className="card-pokemon-text">
+          <img className="img-pokemon" src={imgPokemon[0]} alt=""/>
+          <div className="title-card-pokemon">
+            <h2>{poke.name}</h2>
+            <div className="desc">{infoPokemon}</div>
+            <div className="actions">
+              <button>Comprar <FaShoppingCart /></button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    <div className="card-pokemon">
-      <div className="card-pokemon-text">
-        <img className="img-pokemon" src="https://m.media-amazon.com/images/S/aplus-media/vc/cab6b08a-dd8f-4534-b845-e33489e91240._CR75,0,300,300_PT0_SX300__.jpg" alt=""/>
-        <div className="title-card-pokemon">
-          <h2>Morgan Sweeney</h2>
-          <div className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none
-            in their pants.</div>
-          <div className="actions">
-            <button>Comprar <FaShoppingCart /></button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="card-pokemon">
-      <div className="card-pokemon-text">
-        <img className="img-pokemon" src="https://m.media-amazon.com/images/S/aplus-media/vc/cab6b08a-dd8f-4534-b845-e33489e91240._CR75,0,300,300_PT0_SX300__.jpg" alt=""/>
-        <div className="title-card-pokemon">
-          <h2>Morgan Sweeney</h2>
-          <div className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none
-            in their pants.</div>
-          <div className="actions">
-            <button>Comprar <FaShoppingCart /></button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="card-pokemon">
-      <div className="card-pokemon-text">
-        <img className="img-pokemon" src="https://m.media-amazon.com/images/S/aplus-media/vc/cab6b08a-dd8f-4534-b845-e33489e91240._CR75,0,300,300_PT0_SX300__.jpg" alt=""/>
-        <div className="title-card-pokemon">
-          <h2>Morgan Sweeney</h2>
-          <div className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none
-            in their pants.</div>
-          <div className="actions">
-            <button>Comprar <FaShoppingCart /></button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="card-pokemon">
-      <div className="card-pokemon-text">
-        <img className="img-pokemon" src="https://m.media-amazon.com/images/S/aplus-media/vc/cab6b08a-dd8f-4534-b845-e33489e91240._CR75,0,300,300_PT0_SX300__.jpg" alt=""/>
-        <div className="title-card-pokemon">
-          <h2>Morgan Sweeney</h2>
-          <div className="desc">Morgan has collected ants since they were six years old and now has many dozen ants but none
-            in their pants.</div>
-          <div className="actions">
-            <button>Comprar <FaShoppingCart /></button>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    { pokemon.length }
+    )) }
     
     </div>
   </>;
